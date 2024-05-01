71445645551ca9ae715ee6aa4da617f54699ad4a
Mikołaj Paździor <mikolajpaz@gmail.com>
2024-05-01 10:58:21 +0200

    Second commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
 
+#include "adjacency_list_graph.hpp"
+#include "adjacency_matrix_graph.hpp"
+
+template <typename C>
+void printContainer(const C& container)
+{
+    for (auto & i : container)
+        std::cout << i << ' ';
+    std::cout << std::endl;
+}
+
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    AdjacencyMatrixGraph<int> graph;
+
+    auto v = graph.insertVertex(3);
+    auto w = graph.insertVertex(4);
+
+    graph.insertEdge(*v, *w, 5);
+
+    auto vertices = graph.vertices();
+    printContainer(vertices);
+
+    auto edges = graph.edges();
+    printContainer(edges);
+
+    auto endVert = graph.endVertices(edges[0]);
+    printContainer(endVert);
+
+    auto incidEdg = graph.incidentEdges(vertices[0]);
+    printContainer(incidEdg);
+
+    std::cout << graph.areAdjacent(*v, *w) << std::endl;
+    std::cout << graph.opposite(*w, edges[0]) << std::endl;
+
+    graph.removeVertex(*v);
+
+    std::cout << graph.vertices().size() << ' ' << graph.edges().size() << std::endl;
+
     return 0;
 }
