9815203f8d9e498e0b0a9a2f63690d9bd4984e79
Mikołaj Paździor <mikolajpaz@gmail.com>
2024-05-01 10:58:09 +0200

    Changed

--- src/graph.hpp
+++ src/graph.hpp
@@ -1,31 +1,37 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
 #include <vector>
 
-typedef int sizeType;
+typedef std::size_t sizeType;
 
-class Vertex
-{
-    public:
-        virtual std::vector<Edge> incidentEdges() const = 0;
-        virtual bool isAdjacentTo(const Vertex* const vertex) const = 0;
-};
+template <typename T>
+class Vertex;
 
-class Edge
-{
-    public:
-        virtual std::vector<Vertex> endVertices() const = 0;
-        virtual Vertex& opposite(const Vertex* const vertex) const = 0;
-        virtual bool isAdjacentTo(const Edge* const edge) const = 0;
-        virtual bool isAdjacentOn(const Vertex* const vertex) const = 0;
-};
+template <typename T>
+class Edge;
 
 template <typename T>
 class GraphADT
 {
     public:
-        virtual const std::vector<Vertex>& vertices() const = 0;
-        virtual const std::vector<Edge>& edges() const = 0;
-        virtual const Vertex& insertVertex(const T& element) = 0;
-        virtual const Edge& insertEdge(const Vertex& v, const Vertex& w, const T& element) = 0;
-        virtual void eraseVertex(Vertex* const vertex) = 0;
-        virtual void eraseEdge(Edge* const edge) = 0;
+        // Metody dostepu
+        virtual Vertex<T>* endVertices(const Edge<T>& e) const = 0;
+        virtual Vertex<T>& opposite(const Vertex<T>& v, const Edge<T>& e) const = 0;
+        virtual bool areAdjacent(const Vertex<T>& v, const Vertex<T>& w) const = 0;
+        virtual void replace(const Vertex<T>& v, const T& x) = 0;
+        virtual void replace(const Edge<T>& e, const T& x) = 0;
+        
+        // Metody uaktualniajace
+        virtual void insertVertex(const T& x) = 0;
+        virtual void insertEdge(const Vertex<T>& v, const Vertex<T>& w, const T& x) = 0;
+        virtual void removeVertex(const Vertex<T>& v) = 0;
+        virtual void removeEdge(const Edge<T>& e) = 0;
+
+        // Metody iterujace
+        virtual void incidentEdges(const Vertex<T>& v) const = 0;
+        virtual const std::vector<Vertex<T>>& vertices() const = 0;
+        virtual const std::vector<Edge<T>>& edges() const = 0;
 };
+
+#endif
